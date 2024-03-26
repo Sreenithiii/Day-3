ecaf52c2decb0f5a99ab15c7115ce1f6033a36f1
Sreenithiii <srinithimaheswaran16@gmail.com>
2024-03-26 11:27:31 +0530

    updated

---
+++ script.js
@@ -0,0 +1,42 @@
+
+//Task1
+let obj1 = {name: "person1", age: 5};
+let obj2 = {age: 5, name: "person1"};
+
+if (obj1 === obj2) {
+    console.log("Objects are not equal.");
+} else 
+{
+    console.log("Objects are equal.");
+}
+
+
+//task 2
+
+var request = new XMLHttpRequest();
+request.open ("GET", "https://restcountries.com/v3.1/all", true);
+request.send();
+request.onload=function(){
+    var data = request.response;
+     var result= JSON.parse(data);
+     for (var i = 0; i < result.length; i++) 
+     {
+      console.log(result[i].flags.svg);
+    }
+  }
+
+
+//task 3
+
+var request = new XMLHttpRequest();
+request.open ("GET", "https://restcountries.com/v3.1/all", true);
+request.send();
+request.onload=function(){
+    var data = request.response;
+     var result = JSON.parse(data);
+    for(var i=0;i< result.length;i++)
+    {
+      console.log ("counrtyname:"+" "+result[i].name.common, "region:"+" "+result[i].region.common, "subregion:"+" "+result[i].name.subregion)
+      
+    }
+}
